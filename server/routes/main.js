703a6b9b4891c8f1ec86024076a9190fe5bd74f1
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2023-11-13 22:48:52 +0530

    views and public set with header styling and common theme done

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,9 +2,30 @@ const express = require("express");
 
 const app = express();
 const router = express.Router();
+app.set("view engine", "ejs");
 
 router.get("/", (req, res)=>{
-    res.send("Home page");
+    const data = {
+        title: "Home",
+        descrption: "This is a blog website, where people can create account, write, read and edit their blogs"
+    }
+    res.render("index.ejs", data);
+});
+
+router.get("/about", (req, res)=>{
+    const data = {
+        title: "About",
+        descrption: "This is a blog website, where people can create account, write, read and edit their blogs"
+    }
+    res.render("about.ejs", data);
+});
+
+router.get("/contact", (req, res)=>{
+    const data = {
+        title: "Contacts",
+        descrption: "This is a blog website, where people can create account, write, read and edit their blogs"
+    }
+    res.render("contact.ejs", data);
 });
 
 module.exports = router;
